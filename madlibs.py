5d512ad3cacf375795f51818418db4ce5c71e8d8
Luis Minaya <luis.minaya@gmail.com>
2025-03-04 22:14:46 -0500

    Added game and the madlibs

--- madlibs.py
+++ madlibs.py
@@ -51,6 +51,31 @@ def greet_person():
     return render_template("compliment.html", person=player, compliment=compliment)
 
 
+@app.route("/game")
+def show_madlib_form():
+    """Get user response on if they want to play a game"""
+
+    response = request.args.get("play_game")
+
+    if response == "True":
+        return render_template("game.html")
+    else:
+        return render_template("goodbye.html")
+
+
+@app.route("/madlib")
+def show_madlib():
+    """Show madlibs based on the user's inputs"""
+
+    person = request.args.get("person")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adjective = request.args.get("adjective")
+
+    return render_template("madlib.html", person=person, color=color,
+                           noun=noun, adjective=adjective)
+
+
 if __name__ == "__main__":
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
